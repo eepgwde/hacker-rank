06914ae3013e2b5223061c2de6572b6b999a1079
Mike Slinn <mslinn@micronauticsresearch.com>
2017-08-18 11:20:14 -0700

    -

--- src/main/scala/ArrayLeftRotation.scala
+++ src/main/scala/ArrayLeftRotation.scala
@@ -1,7 +1,10 @@
+import java.io.{BufferedReader, FileInputStream, InputStream, InputStreamReader}
+import java.util.Scanner
 import scala.io.StdIn
 
-/** Minimal memory solution to https://www.hackerrank.com/challenges/ctci-array-left-rotation */
-object ArrayLeftRotation extends App {
+/** Version 1: Minimal memory solution to https://www.hackerrank.com/challenges/ctci-array-left-rotation
+  * Requires one integer per line, which is not what the requirement states */
+object ArrayLeftRotation1 extends App {
   val numInts = StdIn.readInt   // number of integers to read
   val rotations = StdIn.readInt // number of rotations to apply
 
@@ -9,4 +12,27 @@ object ArrayLeftRotation extends App {
 
   (0 to numInts-rotations) foreach { _ => print(StdIn.readInt + " ") } // pass through non-rotated numbers
   rotated foreach { i => print(s"$i ") } // append rotated integers to remainder of stream
+}
+
+/** Version 2: Expects data on two lines */
+class ArrayLeftRotation2(in: InputStream) {
+  val inputStreamReader = new InputStreamReader(in)
+  val bufferedReader = new BufferedReader(inputStreamReader)
+  val line1: String = bufferedReader.readLine.mkString
+  val line2: String = bufferedReader.readLine.mkString
+
+  val sc1: Scanner = new java.util.Scanner(line1)
+  val numInts: Int = sc1.nextInt() // number of integers to read
+  val rotations: Int = sc1.nextInt() // number of rotations to apply
+
+  val sc2: Scanner = new java.util.Scanner(line2)
+  val rotated: Vector[Int] = (1 to rotations).toVector map { _ => sc2.nextInt() } // save rotated integers for later
+
+  (1 to numInts-rotations) foreach { _ => print(sc2.nextInt() + " ") } // pass through non-rotated numbers
+  rotated foreach { i => print(s"$i ") }
+}
+
+object ArrayLeftRotation2 extends App {
+  new ArrayLeftRotation2(new FileInputStream("src/main/resources/data"))
+//  new ArrayLeftRotation2(System.in)
 }
